6f93e48369c09a326e83a41e678a999886e07fe2
souravsingha13 <souravsingha@uatech.biz>
2024-01-04 00:24:55 +0600

    login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import  Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -12,4 +13,16 @@ class RegistrationView(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self,request, format = None):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(email = email,password = password)
+        if user is not None:
+            print(user)
+            login(request, user)
+            return Response("Successfully logged in", status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
         
